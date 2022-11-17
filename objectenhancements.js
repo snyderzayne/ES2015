a6e9916e86fd790206b968dfeecb07f0b54e2c4c
Zayne Snyder <snyderzayne@gmail.com>
2022-11-16 22:13:45 -0500

    added-destructuring

--- objectenhancements.js
+++ objectenhancements.js
@@ -11,7 +11,7 @@ let instructor = {
   [favoriteNumber]: "That is my favorite!"
 }
 
-instructor[favoriteNumber] = "That is my favorite!"
+// instructor[favoriteNumber] = "That is my favorite!"
 
 var instructor = {
     firstName: "Colt",
